efcaf157f555eddfd419391494188115cbc937b3
Dat <htdat0202@gmail.com>
2017-06-11 01:48:53 +0700

    Kick user

--- DoAn_Auction/Controllers/ProductController.cs
+++ DoAn_Auction/Controllers/ProductController.cs
@@ -97,16 +97,35 @@ namespace DoAn_Auction.Controllers
             }
         }
 
+        
         // POST: Product/Detail - Đấu giá
         [HttpPost]
         public ActionResult Detail(int ProID, string PriceSet)
         {
+            if (CurrentContext.IsLogged() == false)
+            {
+                return View();
+            }
+            int UID = (int)CurrentContext.GetCurUser().f_ID;
             Decimal uPriceSet = Convert.ToDecimal(PriceSet);
             using (var ctx = new QLDauGiaEntities())
             {
                 var model = ctx.Auctions
                     .Where(p => p.ProID == ProID)
                     .FirstOrDefault();
+
+                var result = from u in ctx.Users
+                             join ah in ctx.AuctionHistories on u.f_ID equals ah.UserID
+                             where ah.ProID == ProID && ah.Status==true
+                             //into g
+                             select new AuctionHistoryVM
+                             {
+                                 ProID=ah.ProID,
+                                 UserID=ah.UserID,
+                                 Time = ah.Time,
+                                 UserName = u.f_Name,
+                                 Price = ah.Price,
+                             };
                 if (uPriceSet > model.PriceHighest)
                 {
                     model.PriceCurrent = model.PriceHighest + model.Step;
@@ -117,42 +136,98 @@ namespace DoAn_Auction.Controllers
                     var auHis = new AuctionHistory
                     {
                         ProID = model.ProID,
-                        UserID = 1,
-                        Price = model.PriceCurrent - +model.Step,
+                        UserID = UID,
+                        Price = model.PriceCurrent,
                         Time=DateTime.Now,
+                        Status=true,
                     };
                     ctx.AuctionHistories.Add(auHis);
                     ctx.SaveChanges();
+                    ViewBag.AuHis = result.ToList();
                     return View(model);
                 }
                 else
                 {
-                    if(uPriceSet > model.PriceCurrent)
+                    if (uPriceSet == model.PriceHighest)
                     {
-                        model.PriceCurrent = uPriceSet + model.Step;
+                        model.PriceCurrent = uPriceSet;
                         ctx.Entry(model).State = System.Data.Entity.EntityState.Modified;
                         ctx.SaveChanges();
-                        ViewBag.ErrorMsg = "Đã có người đặt giá cao hơn bạn ! Vui lòng đặt giá khác";
+                        ViewBag.ErrorMsg = "Đã có người đặt giá trước bạn bạn ! Vui lòng đặt giá khác";
                         var auHis = new AuctionHistory
                         {
                             ProID = model.ProID,
-                            UserID = 3,
-                            Price = model.PriceCurrent - +model.Step,
+                            UserID = UID,
+                            Price = model.PriceCurrent,
                             Time = DateTime.Now,
+                            Status = true,
                         };
                         ctx.AuctionHistories.Add(auHis);
                         ctx.SaveChanges();
+                        ViewBag.AuHis = result.ToList();
                         return View(model);
+
+                    }
+                    else
+                    {
+                        if (uPriceSet > model.PriceCurrent)
+                        {
+                            model.PriceCurrent = uPriceSet + model.Step;
+                            ctx.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                            ctx.SaveChanges();
+                            ViewBag.ErrorMsg = "Đã có người đặt giá cao hơn bạn ! Vui lòng đặt giá khác";
+                            var auHis = new AuctionHistory
+                            {
+                                ProID = model.ProID,
+                                UserID = UID,
+                                Price = model.PriceCurrent,
+                                Time = DateTime.Now,
+                                Status = true,
+                            };
+                            ctx.AuctionHistories.Add(auHis);
+                            ctx.SaveChanges();
+                            ViewBag.AuHis = result.ToList();
+                            return View(model);
+                        }
                     }
                 }
 
-<<<<<<< HEAD
-=======
                 ViewBag.AuHis = result.ToList();
-				//fix commit @@
->>>>>>> 86c2df32402a5cacdc58e824ffcfaf090b84125a
                 return View(model);
             }
         } 
+
+        // GET: Product/Detail
+        public ActionResult KickUser(int? ProID, int? KUserID)
+        {
+            if (ProID.HasValue == false || KUserID.HasValue == false)
+            {
+                return RedirectToAction("Index", "Product");
+            }
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var auhis = ctx.AuctionHistories
+                    .Where(p => p.ProID == ProID && p.UserID == KUserID)
+                    .ToList();
+                foreach (var p in auhis)
+                {
+                    p.Status = false;
+                    ctx.Entry(p).State = System.Data.Entity.EntityState.Modified;
+                }
+                ctx.SaveChanges();
+                var au = ctx.AuctionHistories.OrderByDescending(p => p.Price)
+                    .Where(p => p.ProID == ProID && p.Status==true).Take(1).FirstOrDefault();
+                var pro = ctx.Auctions.Where(p => p.ProID == ProID).FirstOrDefault();
+                if(pro.Customer==KUserID)
+                {
+                    pro.PriceHighest = au.Price;
+                    pro.PriceCurrent = au.Price;
+                    pro.Customer = au.UserID;
+                    ctx.Entry(pro).State = System.Data.Entity.EntityState.Modified;
+                    ctx.SaveChanges();
+                }
+            }
+            return RedirectToAction("Detail", "Product", new { id = ProID });
+        }
     }
 }
