9a5199868e48203aa29a5989f1610b98406514d0
ductrieu2304 <beatboxrin@gmail.com>
2017-06-15 15:52:03 +0700

    Trang quan tri: xong phan add danh muc

--- DoAn_Auction/Controllers/MCategoryController.cs
+++ DoAn_Auction/Controllers/MCategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using DoAn_Auction.Filters;
+using DoAn_Auction.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,20 +8,47 @@ using System.Web.Mvc;
 
 namespace DoAn_Auction.Controllers
 {
-    [CheckLogin(RequiredPermission=3)]
+    [CheckLogin(RequiredPermission = 3)]
     public class MCategoryController : Controller
     {
         // GET: MCategory
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Current = "Category";
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var cat = ctx.Categories.Where(c => c.ParentID != 0)
+                    .ToList();
+                return View(cat);
+            }
         }
 
 
         // GET: MCategory/Add
         public ActionResult Add()
         {
-            return View();
+            ViewBag.Current = "Category";
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var cat = ctx.Categories.Where(c => c.ParentID == 0)
+                    .ToList();
+                return View(cat);
+            }
+        }
+
+        // POST: MCategory/Add
+        [HttpPost]
+        public ActionResult Add(Category model)
+        {
+            ViewBag.Current = "Category";
+            using (var ctx = new QLDauGiaEntities())
+            {
+                ctx.Categories.Add(model);
+                ctx.SaveChanges();
+                var cat = ctx.Categories.Where(c => c.ParentID != 0)
+                    .ToList();
+                return RedirectToAction("Index", "MCategory");
+            }
         }
     }
 }
