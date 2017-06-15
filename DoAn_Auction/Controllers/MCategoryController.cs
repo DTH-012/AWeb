10fe8e89805ddec5f607b051a5ef45309a61c287
ductrieu2304 <beatboxrin@gmail.com>
2017-06-15 21:01:40 +0700

    Trang quan tri: thêm tinh nang delete và edit

--- DoAn_Auction/Controllers/MCategoryController.cs
+++ DoAn_Auction/Controllers/MCategoryController.cs
@@ -18,7 +18,119 @@ namespace DoAn_Auction.Controllers
 
 
         // GET: MCategory/Add
+        public ActionResult Add()using DoAn_Auction.Filters;
+using DoAn_Auction.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_Auction.Controllers
+{
+    [CheckLogin(RequiredPermission=3)]
+    public class MCategoryController : Controller
+    {
+        // GET: MCategory
+        public ActionResult Index()
+        {
+            ViewBag.Current = "Category";
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var cat = ctx.Categories.Where(c => c.ParentID != 0)
+                    .ToList();
+                return View(cat);
+            }
+        }
+
+
+        // GET: MCategory/Add
         public ActionResult Add()
+        {
+            ViewBag.Current = "Category";
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var cat = ctx.Categories.Where(c => c.ParentID == 0)
+                    .ToList();
+                return View(cat);
+            }
+        }
+
+        // POST: MCategory/Add
+        [HttpPost]
+        public ActionResult Add(Category model)
+        {
+            ViewBag.Current = "Category";
+            using (var ctx = new QLDauGiaEntities())
+            {
+                ctx.Categories.Add(model);
+                ctx.SaveChanges();
+                var cat = ctx.Categories.Where(c => c.ParentID != 0)
+                    .ToList();
+                return RedirectToAction("Index", "MCategory");
+            }
+        }
+
+        // GET: Products/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id.HasValue == false)
+            {
+                return RedirectToAction("Index", "MCategory");
+            }
+
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var cat = ctx.Categories.Where(c => c.CatID == id)
+                    .FirstOrDefault();
+                return View(cat);
+            }
+        }
+
+        // POST: Products/Delete
+        [HttpPost]
+        public ActionResult Delete(int idDelete)
+        {
+
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var catdel = ctx.Categories.Where(c => c.CatID == idDelete)
+                    .FirstOrDefault();
+                ctx.Categories.Remove(catdel);
+                ctx.SaveChanges();
+                return RedirectToAction("Index", "MCategory");
+            }
+        }
+
+        // GET: Products/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id.HasValue == false)
+            {
+                return RedirectToAction("Index", "MCategory");
+            }
+
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var cat = ctx.Categories.Where(c => c.CatID == id)
+                    .FirstOrDefault();
+                return View(cat);
+            }
+        }
+
+        // POST: Products/Edit
+        [HttpPost]
+        public ActionResult Edit(Category model)
+        {
+            using (var ctx = new QLDauGiaEntities())
+            {
+                ctx.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+                return RedirectToAction("Index", "MCategory");
+            }
+        }
+    }
+}
         {
             return View();
         }
