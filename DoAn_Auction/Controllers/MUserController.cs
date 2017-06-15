9cb2286ac3c105a45f5910594b4c13aa961e8dca
ductrieu2304 <beatboxrin@gmail.com>
2017-06-15 10:49:33 +0700

    Trang admin - Quan tri user

---
+++ DoAn_Auction/Controllers/MUserController.cs
@@ -0,0 +1,58 @@
+﻿using DoAn_Auction.Filters;
+using DoAn_Auction.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_Auction.Controllers
+{
+    [CheckLogin(RequiredPermission = 3)]
+    public class MUserController : Controller
+    {
+        // GET: MUser/Index
+        public ActionResult Index()
+        {
+            ViewBag.Current = "User";
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var users = ctx.Users
+                    .OrderBy(u => u.f_ID)
+                    .ToList();
+                return View(users);
+            }
+        }
+        // GET: MUser/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id.HasValue == false)
+            {
+                return RedirectToAction("Index", "MUser");
+            }
+
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var user = ctx.Users.Where(c => c.f_ID == id)
+                    .FirstOrDefault();
+                return View(user);
+            }
+        }
+
+        // POST: MUser/Delete
+        [HttpPost]
+        public ActionResult Delete(int idDelete)
+        {
+
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var userdel = ctx.Users.Where(c => c.f_ID == idDelete)
+                    .FirstOrDefault();
+                userdel.f_Level = 0;
+                ctx.Entry(userdel).State = System.Data.Entity.EntityState.Modified;
+                ctx.SaveChanges();
+                return RedirectToAction("Index", "MUser");
+            }
+        }
+    }
+}
