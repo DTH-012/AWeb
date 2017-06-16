e6d9b60c9e40d95e011272cf98b8c78eabbe9e89
DauNam <quangphuc1994@gmail.com>
2017-06-16 17:06:53 +0700

    load Top 5 sp Hot len trang chu

--- DoAn_Auction/Controllers/HomeController.cs
+++ DoAn_Auction/Controllers/HomeController.cs
@@ -21,5 +21,26 @@ namespace DoAn_Auction.Controllers
             var list = ctx.Auctions.OrderByDescending(p => p.Customer).Take(4).ToList();
             return PartialView("TOPBIDPartial", list);
         }
+		public ActionResult Top5Hot()
+        {
+            using (var ctx = new QLDauGiaEntities())
+            {
+                var result = ctx.AuctionHistories
+                    .Where(a=>a.Status==true)
+                    .GroupBy(p => p.ProID)
+                    .OrderByDescending(a => a.Count())
+                    .Take(5);
+                var proID = new List<int>();
+                var dsSp = new List<Auction>();
+                foreach (var p in result)
+                {
+                    int proid =(int) p.FirstOrDefault().ProID;
+                    proID.Add(p.FirstOrDefault().ProID);
+                    var pro = ctx.Auctions.Where(a => a.ProID == proid).FirstOrDefault();
+                    dsSp.Add(pro);
+                }
+                return PartialView("Top5HotPartial", dsSp);
+            }
+        }
     }
 }
